3d67828d952f01c8a8a89faf13c67084ee3e32d8
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2019-02-07 08:29:09 -0800

    Code cleanup

--- src/data/esc50.py
+++ src/data/esc50.py
@@ -8,22 +8,22 @@ Usage
 
 python esc50.py --log=DEBUG
 
-Extracts features from each of the datasets and puts them into a folder defined by ESC50_SPLITS in the .env file
+Extracts features from each of the datasets and puts them into a folder defined
+by ESC50_SPLITS in the .env file
 
 From here, the files can be loaded using
 
 .. code:: python
 
-	import numpy as np
-	train_data = np.load('train_data.npy')
-	train_labels = np.load('train_labels.npy')
-   
+    import numpy as np
+    train_data = np.load('train_data.npy')
+    train_labels = np.load('train_labels.npy')
+
 """
 
 import numpy as np
 import librosa as lib
 import os
-import pickle
 from pathlib import Path
 from dotenv import load_dotenv
 import logging
@@ -34,12 +34,12 @@ import tqdm
 loglevel = 'WARNING'
 options, remainder = getopt.gnu_getopt(sys.argv[1:], 'l', ['log='])
 for opt, arg in options:
-	if opt in ('-l', '--log'):
-		loglevel = arg
+    if opt in ('-l', '--log'):
+        loglevel = arg
 
 numeric_level = getattr(logging, loglevel.upper(), None)
 if not isinstance(numeric_level, int):
-	raise ValueError('Invalid log level: %s' % loglevel)
+    raise ValueError('Invalid log level: %s' % loglevel)
 logging.basicConfig(level=numeric_level)
 
 load_dotenv()
@@ -52,8 +52,8 @@ dev_path = ESC50_SPLITS_PATH / 'dev'
 test_path = ESC50_SPLITS_PATH / 'test'
 
 for p in (train_path, dev_path, test_path):
-	p.mkdir(parents=True, exist_ok=True)
-	assert p.exists()
+    p.mkdir(parents=True, exist_ok=True)
+    assert p.exists()
 
 # Spectrogram settings
 n_fft = 1024
@@ -77,40 +77,41 @@ test_label = []
 # Go through all files in the folder
 for file_name in tqdm.tqdm(os.listdir(ESC50_RAW_DATA)):
 
-	try:
-		y, sr = lib.load(ESC50_RAW_DATA / file_name, sr=None)
-		#logging.info('Processing %s' % file_name)
+    try:
+        y, sr = lib.load(ESC50_RAW_DATA / file_name, sr=None)
+        #logging.info('Processing %s' % file_name)
 
-		if len(y.shape) > 1:
-		#	logging.info('Mono Conversion') 
-			y = lib.to_mono(y)
+        if len(y.shape) > 1:
+        #   logging.info('Mono Conversion')
+            y = lib.to_mono(y)
 
-		if sr != srate:
-		#	logging.info('Resampling to '+str(srate))
-			y = lib.resample(y,sr,srate)
+        if sr != srate:
+        #   logging.info('Resampling to '+str(srate))
+            y = lib.resample(y,sr,srate)
 
-		mel_feat = lib.feature.melspectrogram(y=y,sr=srate,n_fft=n_fft,hop_length=hop_length,n_mels=128)
-	
-		#Filename decomposed: {Fold} - {ID} - {Take} - {Class}
-		fold, fid, ftake, fclass = file_name.split('-') 
-		fold = int(fold)
-		fclass = int(fclass.split('.')[0])
+        mel_feat = lib.feature.melspectrogram(
+            y=y, sr=srate, n_fft=n_fft, hop_length=hop_length, n_mels=128)
 
-		# Divide the data into three
-		if fold < 4:
-			train_data = np.dstack((train_data, mel_feat))
-			train_label.append(fclass)
-			count_train += 1
-		elif fold == 4:
-			dev_data = np.dstack((dev_data, mel_feat))
-			dev_label.append(fclass)
-			count_dev += 1
-		else:
-			test_data = np.dstack((test_data, mel_feat))
-			test_label.append(fclass)
-			count_test += 1
-	except IOError: # Give me an audio file which I can read!!
-		logging.error(file_name, "did not get coverted!")
+        # Filename decomposed: {Fold} - {ID} - {Take} - {Class}
+        fold, fid, ftake, fclass = file_name.split('-')
+        fold = int(fold)
+        fclass = int(fclass.split('.')[0])
+
+        # Divide the data into three
+        if fold < 4:
+            train_data = np.dstack((train_data, mel_feat))
+            train_label.append(fclass)
+            count_train += 1
+        elif fold == 4:
+            dev_data = np.dstack((dev_data, mel_feat))
+            dev_label.append(fclass)
+            count_dev += 1
+        else:
+            test_data = np.dstack((test_data, mel_feat))
+            test_label.append(fclass)
+            count_test += 1
+    except IOError: # Give me an audio file which I can read!!
+        logging.error(file_name, "did not get coverted!")
 
 # Remove the first element randomly initialized by np.empty
 train_data = np.delete(train_data, 0, 2)
