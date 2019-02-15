1ff043529dec7415b7e13073ced5ff99904bc830
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2019-02-15 11:00:36 -0800

    Trying to make audioset_simple train

--- src/models/audioset_simple.py
+++ src/models/audioset_simple.py
@@ -7,9 +7,14 @@ A simple "get-up-and-running" example of the audioset data.
 import tensorflow as tf
 from tensorflow import keras
 import numpy as np
-import sys
-sys.path.append('..')
-from data.audioset import AudiosetManager
+from dotenv import load_dotenv
+import h5py
+import os
+from pathlib import Path
+load_dotenv()
+
+AUDIOSET_SPLITS_V1 = os.getenv('AUDIOSET_SPLITS_V1')
+AUDIOSET_SPLITS_V1 = Path(AUDIOSET_SPLITS_V1).expanduser()
 
 # TODO:
 # This doesn't quite run yet, but most of the parts are here. Tensorflow is
@@ -17,45 +22,33 @@ from data.audioset import AudiosetManager
 # at the moment.
 
 if __name__ == '__main__':
-    am = AudiosetManager()
 
-    # Build a one-hot matrix (with multiple "one-hots")
-    df = am.get_data_with_single_label()
+    balanced_train_h5 = h5py.File(str(AUDIOSET_SPLITS_V1 / 'balanced_top_5.h5'), 'r')
+    X_train = balanced_train_h5['X'][()]
+    y_train = balanced_train_h5['y'][()].astype(int)
 
-    # Filter out short clips (6 seconds, etc...)
-    keys = [key for key in df.video_id.values
-            if am.get_vggish(key).shape[0] == 10]
-
-    num_labels = max(df.label_id.values) + 1
-    y_train = np.zeros((len(keys), num_labels))
-    for i, key in enumerate(keys):
-        ids = df.query(f'video_id == "{key}"').label_id.values
-        for l in ids:
-            y_train[i][l] = 1
-
-    X_train = np.stack([am.get_vggish(key) for key in keys], axis=0)
-    X_train = np.expand_dims(X_train, axis=-1)
-    print(X_train.shape)
-
+    num_labels = len(y_train[1])
     input_h = X_train.shape[1]
     input_w = X_train.shape[2]
 
     keras.backend.clear_session()
     model = tf.keras.Sequential()
 
-    N_dense = 500
+    N_dense = int(5e2)
     lr = .001
-    N_epochs = 5
-    minibatch_size=32
+    N_epochs = 10
+    minibatch_size = 32
 
-    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Flatten(input_shape=(input_h, input_w, 1)))
     model.add(tf.keras.layers.Dense(N_dense, activation='relu'))
     model.add(tf.keras.layers.Dense(N_dense, activation='relu'))
     model.add(tf.keras.layers.Dense(N_dense, activation='relu'))
-    model.add(tf.keras.layers.Dense(num_labels, activation='softmax'))
+    model.add(tf.keras.layers.Dense(num_labels, activation='sigmoid'))
 
     model.compile(optimizer=tf.keras.optimizers.Adam(lr=lr),
-                  loss='categorical_crossentropy',
+                  loss='binary_crossentropy',
                   metrics=['accuracy'])
 
     model.fit(X_train, y_train, epochs=N_epochs, batch_size=minibatch_size)
+
+    model.save('audioset_simple.h5')
