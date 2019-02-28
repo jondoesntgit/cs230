b3557f6d8fd487e605b71956d7ff353e5da50d0f
Jonathan Wheeler <jonathan.m.wheeler@gmail.com>
2019-02-27 17:10:42 -0800

    Added some example code to upload to AWS

---
+++ src/utils/aws.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import boto3
+from dotenv import load_dotenv
+import os
+load_dotenv()
+
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+
+s3 = boto3.client(
+    's3',
+    # Hard coded strings as credentials, not recommended.
+    aws_access_key_id=AWS_ACCESS_KEY_ID,
+    aws_secret_access_key=AWS_SECRET_ACCESS_KEY
+)
+local_filename = 'file.txt'
+remote_filename = 'file.txt'
+bucket_name = 'cs230-deep-audio'
+
+s3.upload_file(local_filename, bucket_name, remote_filename)
